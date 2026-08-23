764d4e7ed1d3eb4ed33dee222662d06e1e4357dd
Abin Zacharia <abinzacharia1998@gmail.com>
2026-08-23 15:45:40 +0530

    Upgrade Sanity to v6

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,8 +1,7 @@
-import ImageUrlBuilder from '@sanity/image-url';
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url';
 
 import { client } from './client';
 
-const builder = ImageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export const urlFor = (source: SanityImageSource) => builder.image(source);
