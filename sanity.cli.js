6914704e571a15681c5340bcaf3ef3120f57656e
Abin Zacharia <abinzacharia1998@gmail.com>
2026-08-23 16:57:46 +0530

    Persist Sanity deployment app ID

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,6 +2,9 @@ import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   studioHost: 'gulfrakza',
+  deployment: {
+    appId: 'yg7zlq9z9zft5tdht5k0m1w2',
+  },
   api: {
     projectId: 'polviyaz',
     dataset: 'production',
